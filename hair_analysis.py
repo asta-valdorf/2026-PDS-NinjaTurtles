27ede9b0e3d4a33f33aa434ac42aeb0487779ce9
Asta-Valdorf <astp@itu.dk>
2026-04-23 22:41:20 +0200

    improved hair and pen analysis

--- hair_analysis.py
+++ hair_analysis.py
@@ -2,7 +2,6 @@ import cv2
 import numpy as np
 from pathlib import Path
 
-
 # hair detection and removal for light and dark hair
 # group into three groups based on amount of coverage
 
@@ -13,17 +12,15 @@ def hair_coverage(img_gray, kernel_size=9, threshold=10) -> float:
     """
 
     # structuring element shaped as a cross to emphasize thin hair-like structures
-    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (kernel_size, kernel_size))
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (kernel_size, 3))
     
+    # only get dark hair to not get light spots with tophat and overdetect coverage
     blackhat = cv2.morphologyEx(img_gray, cv2.MORPH_BLACKHAT, kernel)
-    tophat = cv2.morphologyEx(img_gray, cv2.MORPH_TOPHAT, kernel)
-    
-    # Combine dark- and light-hair responses
-    combined = cv2.add(blackhat, tophat)
-    
-    # threshold for binary hair mask
-    _, hair_mask = cv2.threshold(combined, threshold, 255, cv2.THRESH_BINARY)
-    hair_mask = cv2.medianBlur(hair_mask, 3)        # reduce small noise
+
+    # segment the pixels where the difference between closing and original image intensities is bigger than 10 
+    _, mask = cv2.threshold(blackhat, threshold, 255, cv2.THRESH_BINARY)
+
+    hair_mask = cv2.medianBlur(mask, 3)        # reduce small noise
     
     # compute coverage ratio
     total_area = img_gray.shape[0] * img_gray.shape[1]
@@ -34,44 +31,34 @@ def hair_coverage(img_gray, kernel_size=9, threshold=10) -> float:
 
 def removeHair_auto(img_org, img_gray, lesion_mask=None):
     """
-    - Selects removal parameters based on coverage level
-    - Chooses between light-hair and dark-hair detection
-    - Inpaints detected hair regions
-
-
-    Args:
-        img_org (_type_): _description_
-        img_gray (_type_): _description_
-        lesion_mask (_type_, optional): _description_. Defaults to None.
-
-    Returns:
-        _type_: _description_
+    Selects removal parameters based on coverage level
+    Chooses between light-hair and dark-hair detection
+    Inpaints detected hair regions
     """
+    
     coverage = hair_coverage(img_gray)
 
-    if coverage < 0.01:
+    if coverage < 0.05:
         return "skip", None, None, img_org.copy(), coverage, 0, 0
 
-    if coverage < 0.08:
+    if coverage < 0.2:
         kernel_size = 9
-        black_threshold = 12
-        top_threshold = 18
+        black_threshold = 11
+        top_threshold = 20
         radius = 3
     else:
         kernel_size = 11
         black_threshold = 15
-        top_threshold = 22
+        top_threshold = 25
         radius = 5
 
 
     # structuring element for hair enhancement
-    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (kernel_size, kernel_size))
-
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (kernel_size, 3))
 
     # compute morphological responses
     blackhat = cv2.morphologyEx(img_gray, cv2.MORPH_BLACKHAT, kernel)
     tophat = cv2.morphologyEx(img_gray, cv2.MORPH_TOPHAT, kernel)
-
 
     # threshold responses to create binary masks
     _, black_mask = cv2.threshold(blackhat, black_threshold, 255, cv2.THRESH_BINARY)
@@ -82,7 +69,7 @@ def removeHair_auto(img_org, img_gray, lesion_mask=None):
     top_score = np.count_nonzero(top_mask)
 
     # select the dominant hair color, only choose light mode if it clearly wins
-    if top_score > 1.3 * black_score:
+    if top_score > 1.2 * black_score:
         mode = "light"
         response = tophat
         mask = top_mask
@@ -92,26 +79,9 @@ def removeHair_auto(img_org, img_gray, lesion_mask=None):
         mask = black_mask
 
     # inpaint detected hair regions
-    img_out = cv2.inpaint(img_org, mask, radius, cv2.INPAINT_TELEA)
+    img_out = cv2.inpaint(img_org, mask, radius, cv2.INPAINT_NS)
 
     return mode, response, mask, img_out, coverage, black_score, top_score
-
-
-# based on histogram
-def label_coverage(coverage) -> str:
-    """
-    Categorize hair coverage level for analysis.
-    Used during exploratory data analysis to group images
-    into low, medium, and high hair coverage regimes.
-    """
-
-    if coverage < 0.01:
-        return "low"
-    elif coverage < 0.08:
-        return "medium"
-    else:
-        return "high"
-
 
 
 def hair_removal(img_bgr):
@@ -137,3 +107,6 @@ def hair_removal(img_bgr):
         return img_bgr
 
     return img_out
+
+
+
